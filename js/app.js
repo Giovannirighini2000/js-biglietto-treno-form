81ad1f93a73719b6db80abea89d75a98d367b0af
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-01 16:14:43 +0100

    feat added macro connections js with html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const formBtnElementGen = document.querySelector('.buttons-js .gen')
+const formElementName = document.querySelector(' input[type="text"]')
+const formElementkm = document.querySelector('.km-form')
+const formElementEta = document.querySelector('select[name="eta-passeggero"]')
+
+console.log(formBtnElementGen,formElementName,formElementkm,formElementEta)
