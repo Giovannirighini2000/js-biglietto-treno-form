6e6253fc620af4e7773eff1791ca1b15563a76fa
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-01 16:36:51 +0100

    feat added price of km setting in html on js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
+// COLLEGAMENTI JS CON HTML
 const formBtnElementGen = document.querySelector('.buttons-js .gen')
 const formElementName = document.querySelector(' input[type="text"]')
 const formElementkm = document.querySelector('.km-form')
-const formElementEta = document.querySelector('select[name="eta-passeggero"]')
+const formElementEta = document.querySelector('select[name="etapasseggero"]')
 
 console.log(formBtnElementGen,formElementName,formElementkm,formElementEta)
+// prezzo fisso km
+const pricekm = 0.21
+// prezzo in base al KM
+formBtnElementGen.addEventListener('click', function () {
+	console.log(' calcola il prezzo del biglietto')
+
+	console.log(formElementkm.value, formElementEta)
+
+	let KmOfElement = formElementkm.value
+	KmOfElement = KmOfElement * pricekm
+    console.log(KmOfElement)
+
+	
+})
+
